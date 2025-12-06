a7c998034166f250718387d6e2325449589749bd
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2025-12-06 18:21:50 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+from aiogram import Router, types
+from aiogram.filters import CommandStart
+
+router = Router()
+
+@router.message(CommandStart())
+async def start(message: types.Message):
+    await message.answer("Hi! I echo everything you say.")
+
+@router.message()            
+async def echo(message: types.Message):
+    await message.send_copy_to(message.chat.id)
